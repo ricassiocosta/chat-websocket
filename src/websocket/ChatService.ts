20aa22dd318d8a62715ad5aad66a579c5219c33d
ricassiocosta <ricassio_costa@outlook.com>
2021-08-14 15:47:23 -0300

    Creating a service to get users

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 io.on("connect", socket => {
   socket.on("start", async data => {
@@ -13,5 +14,14 @@ io.on("connect", socket => {
       name,
       socket_id: socket.id
     })
+
+    socket.broadcast.emit("new_users", user)
+  })
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService)
+    const users = await getAllUsersService.execute()
+
+    callback(users)
   })
 })
