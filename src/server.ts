dbca2b6568df875f18356242a66e2af71c05b4d5
ricassiocosta <ricassio_costa@outlook.com>
2021-08-14 14:22:27 -0300

    adding socket.io into the project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
-import express, { response } from "express"
+import express from "express"
+import path from "path"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 const app = express()
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+const server = createServer(app)
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+  console.log("socket", socket.id)
+})
 
 app.get("/", (request, response) => {
   return response.json({
@@ -8,6 +19,6 @@ app.get("/", (request, response) => {
   })
 })
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("server is running")
 })
