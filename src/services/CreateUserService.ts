56dbec49fad64927f512f411e1b4e7301a579fbb
ricassiocosta <ricassio_costa@outlook.com>
2021-08-14 14:56:59 -0300

    service to create an user implemented

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { injectable } from "tsyringe"
+import { User } from "../schemas/User"
+
+interface CreateUserDTO {
+  email: string;
+  socket_id: string;
+  avatar: string;
+  name: string;
+}
+
+@injectable()
+class CreateUserUseService {
+  async execute({avatar, email, name, socket_id}: CreateUserDTO) {
+    const userAlreadyExists = await User.findOne({
+      email
+    }).exec()
+
+    if(userAlreadyExists) {
+      const user = await User.findOneAndUpdate({
+        _id: userAlreadyExists._id
+      }, {
+        $set: {
+          socket_id,
+          avatar,
+          name
+        }
+      })
+
+      return user
+    } else {
+      const user = await User.create({
+        name,
+        email,
+        avatar,
+        socket_id
+      })
+
+      return user
+    }
+  }
+}
+
+export { CreateUserUseService }
